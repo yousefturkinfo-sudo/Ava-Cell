88cb62ebeb3fc1034ccdc4d720429b9d5294631f
Y T <macbook@Y-T.local>
2025-12-15 20:33:07 +0400

    fix: hardcode path to next binary in build script to fix monorepo resolution

--- dashboard/tailwind.config.js
+++ dashboard/tailwind.config.js
@@ -1,6 +1,5 @@
-import type { Config } from "tailwindcss"
-
-const config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
     darkMode: ["class"],
     content: [
         './pages/**/*.{ts,tsx}',
@@ -75,6 +74,4 @@ const config = {
         },
     },
     plugins: [require("tailwindcss-animate")],
-} satisfies Config
-
-export default config
+}
